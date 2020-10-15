7d6c9b86863f799d87e3d3016a1b19500e0a8464
yolo <sarveshmakane@gmail.com>
2020-10-15 22:49:51 +0530

    END

--- day6_7/homepage.php
+++ day6_7/homepage.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
+    <title>Home page</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Result</title>
@@ -24,5 +25,6 @@
     <a href="StudentLogin.php"><button>Login</button></a>
     <a href="StudentRegister.php"><button>Register</button></a>
     </div>
+
 </body>
 </html>
